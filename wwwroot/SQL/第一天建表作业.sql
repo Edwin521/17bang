c76b73824b3d329c893f11d9ef47565f01aab566
Edwin <1310912217@qq.com>
2020-09-02 20:19:30 +0800

    SQL建表

---
+++ wwwroot/SQL/第一天建表作业.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[EDWIN]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [InviterName] NCHAR(10) NULL, 
+    [InviteCode] NCHAR(10) NULL, 
+    [UserName] NCHAR(10) NULL, 
+    [Password] NCHAR(10) NULL, 
+    [ConfirmPassword] NCHAR(10) NULL, 
+    [InputImageCode] NCHAR(10) NULL
+)
