6c3ea75b08769ea77d0363e1f0b479189d91eeb4
Edwin <1310912217@qq.com>
2020-09-03 17:15:54 +0800

    DATABASE 的  CRUD

---
+++ wwwroot/SQL/数据库的增删改查.sql
@@ -0,0 +1,19 @@
+﻿--//创建一个表 register
+CREATE TABLE [dbo].register
+(
+[ID] INT NOT NULL,
+[name] NVARCHAR(10) NOT NULL,
+[PASSWORD] NVARCHAR(12)  NOT NULL,
+[INviter] NVARCHAR(4) NOT NULL,
+)
+--//增加表中的数据
+ALTER TABLE [register]
+ADD  AGE INT NOT NULL
+--//删除表中的数据
+ALTER TABLE [register]
+DROP COLUMN PASSWORD
+--///修改表中的数据
+ALTER TABLE [register]
+ALTER COLUMN AGE NVARCHAR(10) NULL
+--////删除表
+DROP TABLE [register];
