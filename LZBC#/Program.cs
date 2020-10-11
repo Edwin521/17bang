237a966cd6682d1658213bdff1a3b90c47a56bfd
Edwin <1310912217@qq.com>
2020-10-11 16:51:35 +0800

    页面整理

--- LZBC#/Program.cs
+++ LZBC#/Program.cs
@@ -7,6 +7,51 @@ namespace test2
         static void Main(string[] args)
         {
 
+            ///C#作业
+            //const string greet = "123";
+            // Console.WriteLine(greet);
+
+            //bool gender;
+            //int birthday;
+            //string constellation;
+            //string keyword;
+            //string selfIntroduction;
+
+            //输出两个整数 / 小数的和 / 差 / 积 / 商
+
+            //Console.WriteLine(12+12);
+            //Console.WriteLine(12*12);
+            //Console.WriteLine(12-12);
+            //Console.WriteLine(12/12);
+
+            //Console.WriteLine(12.1+12.1);
+            //Console.WriteLine(12.1-12.1);
+            //Console.WriteLine(12.1*12.1);
+            //Console.WriteLine(12.1/12.1);
+
+            //电脑计算并输出：[(23 + 7)x12-8]÷6的小数值（挑战：精确到小数点以后2位）
+
+            //Console.WriteLine(((23+7)*12-8)/6) ;
+
+            //想一想以下语句输出的结果：
+
+            //int i = 15;
+            //Console.WriteLine(i++);//15
+            //i -= 5;
+            //Console.WriteLine(i);//11
+            //Console.WriteLine(i >= 10);//true
+
+            //Console.WriteLine("i值的最终结果为：" + i); //i值的最终结果为：11
+
+            //int j = 20;
+            //Console.WriteLine($"{i}+{j}={i + j}");//11+20=31
+
+
+            //int a = 10;
+            //Console.WriteLine(a+12);
+            //a -= 1;
+            //Console.WriteLine(a);
+
             ///用户依次由控制台输入：验证码、用户名和密码： 如果验证码输入错误，直接输出：“*验证码错误”；
             // 如果用户名不存在，直接输出：“*用户名不存在”；
             // 如果用户名或密码错误，输出：“*用户名或密码错误”
