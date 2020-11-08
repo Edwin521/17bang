69604e74491fa8e704b4125fb4ff70930c30bd28
Edwin <1310912217@qq.com>
2020-11-08 19:48:57 +0800

    （自己练习）类中实现接口，使一个普通类可以使用foreach遍历。

--- MySelf/Program.cs
+++ MySelf/Program.cs
@@ -1,31 +1,59 @@
 ﻿using System;
+using System.Collections;
 
 namespace MySelf
 {
-   public class human
+    public class human
     {
-        public  double age { get; set; }
+
+
+
 
         static void Main(string[] args)
         {
 
+            students lzb = new students();
+            foreach (var item in lzb)
+            {
+                Console.WriteLine(item);
+            }
 
-            //findPrimeNum(10, 30);
+        }
+    }
 
-            //double result = Math.Round(0.3333, 2);
-            //Console.WriteLine(result);
 
-            //Program.publish();
-            //student lzb = new student();
-            //lzb.age = 18;
-            //student clone = lzb;
-            //clone.age = 20;
-         
+
+    class students : IEnumerable///实现IEnumerable接口就可以躲过编译时检查，不报错，但运行时会报错
+    {                            ///不写后面这个接口名也没事，只要在类中实现了GetEnumerator()方法就可以了
+
+
+        public class StudentsEnumerator : IEnumerator  ///类中类实现返回一个
+        {
+            public object[] ages = new object[]  { 12,34,56,75,332};
+            private int index=-1;
+            public object Current
+            {
+                get
+                {
+                    return ages[index];
+                }
+            }
+
+            public bool MoveNext()
+            {
+            index++;
+                return index < ages.Length;
+            }
+
+            public void Reset()
+            {
+                throw new NotImplementedException();
+            }
         }
 
-        public  int  grow( int age)
+        public IEnumerator GetEnumerator()
         {
-            return age++;
+            return new StudentsEnumerator();
         }
     }
 }
