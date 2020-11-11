8f4622e0ec2cf55a17c2d5ccfd4f2d28bc8934bf
Edwin <1310912217@qq.com>
2020-11-11 15:21:50 +0800

    改造Entity类，让其Id可以为任意类型

--- LZBC#/Entity.cs
+++ LZBC#/Entity.cs
@@ -5,8 +5,9 @@ using System.Text;
 namespace LZBC
 {
     //为之前所有类（含User、HelpMoney等）抽象一个基类：Entity，包含一个只读的Id属性。试一试，Suggest能有Id属性么？ 
-   public class Entity
+    //改造Entity类，让其Id可以为任意类型 
+    public class Entity <T>
     {
-        public  int id { get; }
+        public   T id { get; }
     }
 }
